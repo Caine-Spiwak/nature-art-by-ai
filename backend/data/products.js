63088c3c462c818ab45bec394a38d22006b8841e
Caine <143353083+Caine-Spiwak@users.noreply.github.com>
2024-01-23 17:04:41 -0800

    Initial Commit

---
+++ backend/data/products.js
@@ -0,0 +1,103 @@
+const products = [
+  {
+    name: 'A Way Out',
+    image: '/images/1.png',
+    description:
+      `The artist has used bold brushstrokes and vibrant colors to create this unique interpretation of nature. 
+      The strokes are thick and textured, giving depth and dimension to the tree's trunk and branches. 
+      Shades of green, brown, yellow, and orange blend together seamlessly to form leaves that appear almost lifelike in their movement.`,
+    brand: 'nature by AI',
+    category: 'Forest',
+    price: 39.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Whispers of the Forest',
+    image: '/images/2.png',
+    description:
+      `Delicate greens and earthy browns dance together, forming an intricate tapestry that evokes the mysterious beauty of a secluded forest bathed in dappled sunlight.`,
+    brand: 'nature by Ai',
+    category: 'Forest',
+    price: 39.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Sylvan Serenity',
+    image: '/images/3.png',
+    description:
+      `Mossy greens and muted browns intertwine, creating a serene portrayal of a hidden glade where ancient trees stand tall, and the air is filled with the gentle whispers of nature.`,
+    brand: 'nature by Ai',
+    category: 'Forest',
+    price: 39.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Enchanted Canopy',
+    image: '/images/7.png',
+    description:
+      `Earthy tones weave through the canvas, forming an enchanted canopy that transports viewers to a magical forest realm, where sunlight filters through the leaves like a spell.`,
+    brand: 'nature by Ai',
+    category: 'Forest',
+    price: 39.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Mystic Woodland Melody',
+    image: '/images/4.png',
+    description:
+      `Harmonious hues of forest greens and deep browns harmonize in a captivating melody, capturing the mystical ambiance of a woodland where nature's secrets are whispered in the wind.`,
+    brand: 'nature by Ai',
+    category: 'Forest',
+    price: 39.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Secret Grove Sonata',
+    image: '/images/8.png',
+    description:
+      `The canvas orchestrates a visual sonata with its subtle greens and browns, unveiling the beauty of a secret grove where sunlight plays a soft melody on the leaves and forest creatures harmonize.`,
+    brand: 'nature by Ai',
+    category: 'Forest',
+    price: 39.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Ephemeral Verdancy',
+    image: '/images/6.png',
+    description:
+      `A fleeting moment of verdant beauty unfolds on the canvas, capturing the ephemeral essence of a forest bathed in soft sunlight, where each stroke tells a story of nature's quiet resilience.`,
+    brand: 'nature by Ai',
+    category: 'Forest',
+    price: 39.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Dunes in Harmony',
+    image: '/images/5.png',
+    description:
+      `Soft ochre and warm beige hues converge on the canvas, creating a tranquil portrayal of endless sandy dunes. Each stroke captures the subtle dance of sunlight, unveiling the harmonious beauty of a desert landscape, where the wind whispers secrets across the rolling hills of sand.`,
+    brand: 'nature by Ai',
+    category: 'Forest',
+    price: 39.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+ 
+]
+
+export default products
